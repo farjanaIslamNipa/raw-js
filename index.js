dfacef48d77bd61e9742c6da93a8225cc7811430
farjanaIslamNipa <farjana.jatri@gmail.com>
2023-09-05 15:43:24 +0600

    abdc

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-function getCommon(a, b) {
-    let arr1 = a.sort((a, b) => (a - b));                    
-    let arr2 = b.sort((a, b) => (a - b));
+// function getCommon(a, b) {
+//     let arr1 = a.sort((a, b) => (a - b));                    
+//     let arr2 = b.sort((a, b) => (a - b));
 
-    let common = [];                  
-    let i = 0, j = 0;    
+//     let common = [];                  
+//     let i = 0, j = 0;    
   
-    while(i<arr1.length && j<arr2.length) {
+//     while(i<arr1.length && j<arr2.length) {
       
-      if(arr1[i] == arr2[j]) {        
-        common.push(arr1[i]);
-        i++;
-        j++;
-        console.log('common');
-      }
-      else if(arr1[i] < arr2[j]) {  
-        i++; 
-        console.log('arr1');                       
-      }                             
-      else {
-        j++;
-        console.log('arr2');    
-      }
-    }
+//       if(arr1[i] == arr2[j]) {        
+//         common.push(arr1[i]);
+//         i++;
+//         j++;
+//         console.log('common');
+//       }
+//       else if(arr1[i] < arr2[j]) {  
+//         i++; 
+//         console.log('arr1');                       
+//       }                             
+//       else {
+//         j++;
+//         console.log('arr2');    
+//       }
+//     }
     
-    return common;
+//     return common;
       
+//     }
+  // ----------------------------------------------
+
+  // 
+  var expect = function(val) {
+    return {
+      toBe: (val2) => {
+        if(val2 !== val) throw new Error('Not Equal')
+        else return true;
+      },
+      notToBe: (val2) => {
+        if(val === val2) throw new Error('Equal')
+        else return true;
+      }
     }
-  
+ };
 
-  //  const makePairs = (object) => Object.keys(object).map((el) => [el, object[el]]);
+ console.log(expect(5).toBe(null));
+ 
+ /**
+  * expect(5).toBe(5); // true
+  * expect(5).notToBe(5); // throws "Equal"
+  */
 
-  //  const makePairs =(object) => Object.keys(object).map(el => [el, object[el]])
 
-  //  console.log(makePairs({ a: 1, b: 2 }));
-
-  const myFunc = (obj) => {
-    const objectKeys = Object.keys(obj);
-    if(objectKeys.length === 0) return true;
-
-    return !objectKeys.filter((key) => obj[key] || obj[key] === 0 || obj[key] === false).length;
-     
-  }
-  console.log(myFunc({ a: undefined}));
