a1fcd37d61b28d8961653a65493d337ef60d9799
farjanaIslamNipa <nipa.farzana87@gmail.com>
2023-10-12 11:24:35 +0600

    problem solving

--- index.js
+++ index.js
@@ -42,28 +42,16 @@ const { boolean } = require("yargs");
 // }
 // console.log(myFunc({ a: 1, b: 2 }, 'b'));
 
-function vowelsAndConsonants(s) {
+
+function regexVar(s) {
+  // console.log(typeof s);
+  if (typeof s === 'string'){
+    return s.split("").reverse().join("");
+  }else{
+    
+  }
   
-    const vowelArr = []
-    const consArr = []
-
-    for (let i = 0; i < s.length; i++) {
-      const element = s[i];
-      if(element === 'a' || element === 'e' || element === 'i' || element === 'o' || element === 'u'){
-        vowelArr.push(element)
-      }else{
-        consArr.push(element)
-      }
-    }
-    let result = [...vowelArr, ...consArr]
-
-    for (let j = 0; j < result.length; j++) {
-      const element = result[j];
-      console.log(element);
-      
-    }
-
-   
 }
-vowelsAndConsonants('Javascriptloops')
+console.log(regexVar("hello"));
+
 
